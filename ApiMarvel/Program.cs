2616539e3e2215df829e8d0c4f8690e2bf0b539b
MarcosSeixas <seixas.developer@gmail.com>
2022-06-29 19:57:00 -0300

    first commit using visual studio

--- ApiMarvel/Program.cs
+++ ApiMarvel/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class ApiMarvel
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello friend.");
+    }
+}
